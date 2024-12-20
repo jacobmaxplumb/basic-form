76b56b396b65252a22eb2f213d1af333d5a51b37
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-12-19 20:03:14 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -8,55 +8,66 @@ const initialFormValues = {
   animals: [],
 };
 
+const animals = [
+  { animal_id: "1", animal_name: "dog" },
+  { animal_id: "2", animal_name: "cat" },
+  { animal_id: "3", animal_name: "bird" },
+  { animal_id: "4", animal_name: "fish" },
+];
+
 function App() {
   const [formValues, setFormValues] = useState(initialFormValues);
 
   const handleTextChange = (e) => {
-    // e.target.name = 'fullName'
-    // e.target.value = 'a'
-    // {fullName: '', shirtSize: '', animals: [], fullName: 'a'}
-    // {shirtSize: '', animals: [], fullName: 'a'}
-    const {name, value} = e.target;
-    setFormValues({...formValues, [name]: value})
-  }
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
   const handleCheckboxChange = (e) => {
-    const {checked, name} = e.target;
+    const { checked, name } = e.target;
     if (checked) {
-      // {fullName: '', shirtSize: '', animals: ['dog']}
-      // {fullName: '', shirtSize: '', animals: ['dog'], animals: ['dog', 'cat']}
-      // {fullName: '', shirtSize: '', animals: ['dog', 'cat']}
-      setFormValues({...formValues, animals: [...formValues.animals, name]})
+      setFormValues({ ...formValues, animals: [...formValues.animals, name] });
     } else {
-      setFormValues({...formValues, animals: formValues.animals.filter(a => a !== name)})
+      setFormValues({
+        ...formValues,
+        animals: formValues.animals.filter((a) => a !== name),
+      });
     }
-  }
+  };
 
   return (
     <div>
       <div>
-        <input onChange={handleTextChange} name="fullName" value={formValues.fullName} placeholder="full name" />
+        <input
+          onChange={handleTextChange}
+          name="fullName"
+          value={formValues.fullName}
+          placeholder="full name"
+        />
       </div>
       <div>
-        <select name="shirtSize" onChange={handleTextChange} value={formValues.shirtSize}>
+        <select
+          name="shirtSize"
+          onChange={handleTextChange}
+          value={formValues.shirtSize}
+        >
           <option value="">select a shirt size</option>
           <option value="S">Small</option>
           <option value="M">Medium</option>
           <option value="L">Large</option>
         </select>
       </div>
-      <div>
-        <input name="dog" onChange={handleCheckboxChange} checked={formValues.animals.includes('dog')} type="checkbox"/> Dog
-      </div>
-      <div>
-        <input name="cat" onChange={handleCheckboxChange} checked={formValues.animals.includes('cat')} type="checkbox"/> Cat
-      </div>
-      <div>
-        <input name="fish" onChange={handleCheckboxChange} checked={formValues.animals.includes('fish')} type="checkbox"/> Fish
-      </div>
-      <div>
-        <input name="bird" onChange={handleCheckboxChange} checked={formValues.animals.includes('bird')} type="checkbox"/> Bird
-      </div>
+      {animals.map((animal) => (
+        <div key={animal.animal_id}>
+          <input
+            name={animal.animal_id}
+            onChange={handleCheckboxChange}
+            checked={formValues.animals.includes(animal.animal_id)}
+            type="checkbox"
+          />{" "}
+          {animal.animal_name}
+        </div>
+      ))}
       <div>
         <button>Submit</button>
       </div>
